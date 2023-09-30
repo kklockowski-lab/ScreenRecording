704f7ec623074ab809497dcf3b45155947b88ccc
kklockowski <k.j.klockowski@gmail.com>
2023-09-30 13:49:33 +0200

    Dodanie dllimport i obsluga zamykania konsoli wg stackoverflow.

--- RecordingScreen/Recording.cs
+++ RecordingScreen/Recording.cs
@@ -1,5 +1,7 @@
 ﻿using NAudio.Wave;
+using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace RecordingScreen
 {
@@ -7,14 +9,27 @@ namespace RecordingScreen
     {
 
         private const string FfmpegPath = "ffmpeg.exe"; // Podaj pełną ścieżkę do pliku ffmpeg.exe
-        private const string otupAviResult = "output\\result.avi"; // Ścieżka do pliku wynikowego
+        private const string otupAviResult = "output\\result.mp4"; // Ścieżka do pliku wynikowego
         private const string otupWavResult = "output\\result.wav"; // Ścieżka do pliku wynikowego
-        private const string outputResultAvu = "output\\final_video.avi"; // Ścieżka do pliku wynikowego
+        private const string outputResultAvu = "output\\final_video.mp4"; // Ścieżka do pliku wynikowego
 
 
         private WaveInEvent waveSource;
         private WaveFileWriter waveFile;
         Process process;
+
+        internal const int CTRL_C_EVENT = 0;
+        [DllImport("kernel32.dll")]
+        internal static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool AttachConsole(uint dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        internal static extern bool FreeConsole();
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate HandlerRoutine, bool Add);
+        // Delegate type to be used as the Handler Routine for SCCH
+        delegate Boolean ConsoleCtrlDelegate(uint CtrlType);
+
 
         public void StartRecording(string filePath)
         {
@@ -59,15 +74,14 @@ namespace RecordingScreen
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = FfmpegPath,
-                    //Arguments = $"-f gdigrab -framerate 30 -i desktop -b:v 3000k -b:a 192k {otupAviResult}",
-                    Arguments = $"-f gdigrab -framerate 30 -i desktop -q:v 2 -s 1920x1080 {otupAviResult}",
+                    Arguments = $"-f gdigrab -framerate 30 -i desktop -c:v libopenh264 -crf 18 -s 1920x1080 {otupAviResult}",
                     UseShellExecute = false,
                     CreateNoWindow = true,
                     RedirectStandardOutput = false,
                     RedirectStandardError = false
                 }
             };
-
+            process.StartInfo.RedirectStandardInput = true;
             process.Start();
             StartRecording(otupWavResult);
         }
@@ -79,27 +93,53 @@ namespace RecordingScreen
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = FfmpegPath,
-                    Arguments = $"-i {otupAviResult} -i {otupWavResult} -c copy {outputResultAvu}",
-                    //Arguments = $"-i output\\result.avi -i output\\result.wav -c copy output\\final_video.avi",
+                    Arguments = $"-i {otupAviResult} -i {otupWavResult} -c:v copy -c:a aac -strict experimental {outputResultAvu}",
                     UseShellExecute = false,
                     CreateNoWindow = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
+                    RedirectStandardOutput = false,
+                    RedirectStandardError = false
                 }
             };
 
             process.Start();
-            // process.WaitForExit();
-            ;
+            process.WaitForExit();
+            
         }
 
         public void Stop()
         {
             StopRecording();
-            process.Kill();
-            process.WaitForExit();
+
+
+            if (AttachConsole((uint)process.Id))
+            {
+                SetConsoleCtrlHandler(null, true);
+                try
+                {
+                    if (!GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0))
+                        process.WaitForExit();
+                }
+                finally
+                {
+                    SetConsoleCtrlHandler(null, false);
+                    FreeConsole();
+                }
+            }
 
             JoinWavAndAvi();
+        }
+
+        public void KillFFMpegProcess()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "taskkill",
+                Arguments = "/F /IM ffmpeg.exe",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            Process.Start(psi);
         }
 
     }
